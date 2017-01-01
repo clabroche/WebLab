5b5892ffd902415e382d7f24cc12fa3bddceeaba
Xavier Chopin <xchopin54@gmail.com>
2017-01-01 21:59:36 +0100

    Set up the Framework

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
+/**
+ * SERVER.JS
+ * MAIN FILE
+ * @author Xavier CHOPIN, Corentin LABROCHE, David LEBRUN, Maxence ANTOINE
+ * @license    MIT (3-clause)
+ * @copyright  (c) 2016-2017 University of Lorraine
+ * @link       http://github.com/tpcisiie/weblab
+ */
+
+
+/**
+  -------------------------------------
+ | Settings                            |
+ |-------------------------------------
+ |
+ | Initialization of ExpressJS and Twig
+ |
+ */
+
 let app = require('express')();
 
+
+app.set('twig options', {
+    strict_variables: false,
+});
+
+/**
+  -------------------------------------
+ | Routes                              |
+ |-------------------------------------
+ |
+ | Routes of the web application
+ |
+ */
+
 app.get('/', (request, response) => {
-    response.send('Hello world !');
+    response.render('app/home.twig', {
+        message: 'Hello Twig this is World'
+    });
 });
 
 app.listen(8080);
