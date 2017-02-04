2c6c4c9af5057ec3b6c72536d06f947932b8f61f
iryu54 <corentinlabroche@gmail.com>
2017-02-04 15:09:28 +0000

    commentaire sur socket io pour le client

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,18 +1,26 @@
 var socket = io('http://localhost:8081');
-socket.on('slaveConnection', function(data) {
-    addSlave(data.port, data.ip);
-});
 
-socket.on('slaveDisconnect', function(data) {
-    $("#" + data).remove();
-});
-
+// Recuperation des serveurs deja connecte
 socket.on('slaveInit', function(data) {
+    // Pour chaque esclave on met a jour la vue
     data.forEach(elem => {
         addSlave(elem.port, elem.ip);
     });
 });
 
+// Lors d'une nouvelle connexion d'un esclave
+socket.on('slaveConnection', function(data) {
+    // On ajoute l'esclave a la vue
+    addSlave(data.port, data.ip);
+});
+
+// Lors de la deconnxion d'un esclave
+socket.on('slaveDisconnect', function(data) {
+    //On supprime l'esclave de la vue
+    $("#" + data).remove();
+});
+
+// Template pour ajouter un esclave
 function addSlave(port, ip) {
     if (!$("#" + port).length) {
         $('#slaveContainer').append("<div class='slave' id='" + port + "'>" + ip + "<br>" + port + "</div>");
