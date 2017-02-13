675d05066bcbadc931dbe35c0d7b42f6e0cacb6a
iryu54 <corentinlabroche@gmail.com>
2017-02-13 04:30:54 +0000

    master = reorganisation en package + socket io sur reponse + middlewares

--- src/Master/routes/index.js
+++ src/Master/routes/index.js
@@ -4,6 +4,37 @@ let hardwareController = require('../../../src/Master/Controllers/HardwareContro
 let router = express.Router();
 
 router.get('/', (req, res, next) => {
+
+
+  var slaves = [];
+  // Lorsque un connection quelconque apparait
+  res.io.on('connection', function(socket) {
+      socket.emit('slaveInit', slaves);
+      //Lors de la connection d'un serveur
+      socket.on('slaveConnection', function(slave) {
+          // On enregistre l'esclave
+          slaves.push({
+              ip: slave.ip,
+              port: slave.port,
+              id: socket.id
+          });
+          // On notifie la vue qu'un esclave s'est connecté
+          socket.broadcast.emit('slaveConnection', slave);
+          // Lors de la deconnexion
+          socket.on("disconnect", function() {
+              // On parcours le tableau des esclaves pour le supprimer de la liste
+              slaves.forEach((slave, index, object) => {
+                  if (slave.id == socket.id) {
+                      object.splice(index, 1);
+                      // On notifie la vue de la deconnexion
+                      socket.broadcast.emit('slaveDisconnect', slave.port);
+                  }
+              });
+          });
+      });
+  });
+
+
     new homeController(req, res, next).index();
 });
 
