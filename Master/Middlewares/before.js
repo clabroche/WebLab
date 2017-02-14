e13a35797f8a8831d3d75ae42d3edf008f3f11bb
iryu54 <corentinlabroche@gmail.com>
2017-02-14 01:01:04 +0000

    Création d'un logo

--- Master/Middlewares/before.js
+++ Master/Middlewares/before.js
@@ -2,6 +2,7 @@ let path = require('path')
 let logger = require('morgan')
 let cookieParser = require('cookie-parser')
 let bodyParser = require('body-parser')
+var favicon = require('serve-favicon')
 
 function middlewares (express, app, io) {
   app.use((req, res, next) => {
