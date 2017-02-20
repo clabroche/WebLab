a09abf34442e78ed0b81ecd7c7eb02f3c43d248c
xchopin <xchopin54@gmail.com>
2017-02-21 00:54:41 +0100

    Added new Slave as a slot

--- Master/public/js/socket/home.js
+++ Master/public/js/socket/home.js
@@ -39,30 +39,48 @@ let countServerId = (() => {
  */
 function addSlave (port, ip) {
   if (!$('#' + port).length) {
+    let serverName = 'Server #' + countServerId()
     let icon = $('<i>').addClass('ui disk outline icon')
-    let title = $('<h3>').text('Server #' + countServerId()).prepend(icon)
-    let slave = $('<div>').addClass('slave').prop('id', port).click((event) => {
-      $.getJSON('http://' + ip + ':' + port + '/hardware', (config, textStatus) => {
-        console.log(config)
-        let cutePercent = config.cpuUsage[0].toFixed(2) * 1.8  // because when it's too low we don't see the text and the cpu is always low lol
-        let cpu = $('<div>').addClass('ui green active progress')
-              .append($('<div>').attr('style', 'transition-duration: 300ms; width:' + cutePercent + '%;')
-                  .addClass('bar')
-                  .append($('<div>').addClass('progress').text(config.cpuUsage[0].toFixed(1) + '%'))
-              ).append($('<div>').addClass('label').text('CPU : ' + config.cpus[0].model + ' x' + config.cpus.length))
-        let totalRAM = (config.totalmem / 1000000000).toFixed(2)
-        let currentRAM = (config.freemem / 1000000000).toFixed(2)
-        let percent = Math.round((currentRAM * 100) / totalRAM)
-        let ram = $('<div>').addClass('ui orange active progress')
-              .append($('<div>').attr('style', 'transition-duration: 300ms; width:' + percent + '%;')
-                  .addClass('bar')
-                  .append($('<div>').addClass('progress').text(percent + '%'))
-              ).append($('<div>').addClass('label').text('Memory : ' + currentRAM + 'GB /' + totalRAM + 'GB'))
+    let title = $('<h3>').text(serverName).prepend(icon)
+    let slave = $('<div>').addClass('slave').prop('id', port)
+    // .click((event) => {
+    $.getJSON('http://' + ip + ':' + port + '/hardware', (config, textStatus) => {
+      console.log(config)
+      let cutePercent = config.cpuUsage[0].toFixed(2) * 1.8  // because when it's too low we don't see the text and the cpu is always low lol
+      let cpu = $('<div>').addClass('ui green active progress')
+            .append($('<div>').attr('style', 'transition-duration: 300ms; width:' + cutePercent + '%;')
+                .addClass('bar')
+                .append($('<div>').addClass('progress').text(config.cpuUsage[0].toFixed(1) + '%'))
+            )
+      let totalRAM = (config.totalmem / 1000000000).toFixed(2)
+      let currentRAM = (config.freemem / 1000000000).toFixed(2)
+      let percent = Math.round((currentRAM * 100) / totalRAM)
+      let ram = $('<div>').addClass('ui orange active progress')
+           .append($('<div>').attr('style', 'transition-duration: 300ms; width:' + percent + '%;')
+               .addClass('bar')
+               .append($('<div>').addClass('progress').text(percent + '%'))
+           ).append($('<div>').addClass('label').text('Memory : ' + currentRAM + 'GB /' + totalRAM + 'GB'))
 
-        slave.append(cpu)
-        slave.append(ram)
-      })
+      slave.append(cpu.clone().append($('<div>').addClass('label').text('CPU : ' + config.cpus[0].model + ' x' + config.cpus.length)) )
+      slave.append(ram)
+      $('#container1').append(createHTMLCard(serverName, cpu.clone().append($('<div>').addClass('label').text('CPU : ' + config.cpus[0].model)), ram))
     })
+
     $('#slaveContainer').append(slave.append(title))
   }
+}
+
+function createHTMLCard (serverName, cpuBar, ramBar) {
+  let headerContent = '<i class="disk green outline icon"></i> ' + serverName
+  let header = '<div class="header">' + headerContent + '</div>'
+  let description = '<div class="description"> ' + cpuBar[0].outerHTML + '<br/>' + ramBar[0].outerHTML + '</div>'
+  let body = '<div class="meta"> Available </div>' + description
+
+  let action1 = '<div class="ui basic green button"> Pause </div>'
+  let action2 = '<div class="ui basic red button"> Stop </div>'
+  let actions = action1 + action2
+  let buttons = '<div class="extra content"> <div class="ui two buttons">' + actions + '</div> </div>'
+  let content = header + body
+
+  return $('<div>').addClass('card').append($('<div>').addClass('content').append(content)).append(buttons)
 }
