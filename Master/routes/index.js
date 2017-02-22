417d6b899ebd575cad6561a873bba6237bdd90a6
iryu54 <corentinlabroche@gmail.com>
2017-02-22 12:26:48 +0000

    Externalisation de la gestion de la liste de slave dans un module externe pour l'accessibilité et la lisibilité

--- Master/routes/index.js
+++ Master/routes/index.js
@@ -1,37 +1,11 @@
 let express = require('express')
 let HomeController = require('../Controllers/HomeController')
+let slaves = require('../Models/Slaves')
 let HardwareController = require('../Controllers/HardwareController')
 let router = express.Router()
 
 router.get('/', (req, res, next) => {
-  var slaves = []
-  // Lorsque un connection quelconque apparait
-  res.io.on('connection', (socket) => {
-    socket.emit('slaveInit', slaves)
-    // Lors de la connection d'un serveur
-    socket.on('slaveConnection', (slave) => {
-      // On enregistre l'esclave
-      slaves.push({
-        ip: slave.ip,
-        port: slave.port,
-        id: socket.id
-      })
-      // On notifie la vue qu'un esclave s'est connecté
-      socket.broadcast.emit('slaveConnection', slave)
-      // Lors de la deconnexion
-      socket.on('disconnect', () => {
-        // On parcours le tableau des esclaves pour le supprimer de la liste
-        slaves.forEach((slave, index, object) => {
-          if (slave.id === socket.id) {
-            object.splice(index, 1)
-            // On notifie la vue de la deconnexion
-            socket.broadcast.emit('slaveDisconnect', slave.port)
-          }
-        })
-      })
-    })
-  })
-
+  slaves.addSlave(res)
   new HomeController(req, res, next).index()
 })
 
