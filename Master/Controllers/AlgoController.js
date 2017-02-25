48dd1c8c6eadbe08a0a965b3ffd4db54511c518f
iryu54 <corentinlabroche@gmail.com>
2017-02-25 15:07:55 +0000

    Stockage de l'algo dans le module d'algo

--- Master/Controllers/AlgoController.js
+++ Master/Controllers/AlgoController.js
@@ -1,5 +1,6 @@
 let Controller = require('./controller')
 let slaves = require('../Models/Slaves')
+let algo = require('../Models/Algo')
 let rp = require('request-promise')
 let AlgoController = class AlgoController {
 
@@ -12,6 +13,11 @@ let AlgoController = class AlgoController {
 
   upload () {
     // eval(JSON.parse(this.req.body.algo))
+    algo.add(this.req.body.algo)
+    console.log(algo.get())
+  }
+
+  launch () {
     let slave = slaves.available()[0]
     console.log(slave.port)
     if (slave !== undefined) {
