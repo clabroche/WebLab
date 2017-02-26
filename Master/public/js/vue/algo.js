c2b1b62e60b2a0e579997aee81a92ebad027c222
iryu54 <corentinlabroche@gmail.com>
2017-02-26 23:35:02 +0000

    Separation de la logique et des vues du coté client

--- Master/public/js/vue/algo.js
+++ Master/public/js/vue/algo.js
@@ -1,36 +1,3 @@
-let socket = io('http://localhost:8081')
-// Recuperation des serveurs deja connecte
-socket.on('slaveInit', (slaves) => {
-  // Pour chaque esclave on met a jour la vue
-  slaves.forEach(slave => {
-    addSlave(slave.port, slave.ip)
-  })
-})
-
-/**
- * When a slave connects to the application
- */
-socket.on('slaveConnection', (slave) => {
-  console.log(slave)
-  addSlave(slave)
-})
-
-/**
- * Function when a slave disconnect from the application
- */
-socket.on('slaveDisconnect', (slave) => {
-  $('.' + slave.id).remove()
-})
-
-$('#uploadAlgo').click(() => {
-  let algo = JSON.stringify(editor.getValue())
-  $.post('/uploadAlgo', {algo: algo}, function (json, textStatus) {})
-})
-
-$('body').on('click', '.launch', function (event) {
-  let iteration = $(this).parents().find('.iteration').val()
-  $.post('/launchAlgo', {server: $(this).prop('id'), iteration: iteration}).done((data) => {}).fail((data) => {})
-})
 
 /**
  * Function to add a Slave to the HTML Page
