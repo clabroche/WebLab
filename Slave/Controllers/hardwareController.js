90f164fc34716b1bc597310933bbd252a79507f5
iryu54 <corentinlabroche@gmail.com>
2017-02-27 18:06:46 +0000

    Reparation de l'affichage de la ram + cpu

--- Slave/Controllers/hardwareController.js
+++ Slave/Controllers/hardwareController.js
@@ -10,21 +10,29 @@ let hardwareController = class hardwareController {
     this.controller = new Controller(req, res, next)
   }
   getinfos () {
-    let infos = {
-      'architecture': os.arch(),
-      'cpus': os.cpus(),
-      'homedir': os.homedir(),
-      'hostname': os.hostname(),
-      'freemem': os.freemem(),
-      'networkInterfaces': os.networkInterfaces(),
-      'platform': os.platform(),
-      'release': os.release(),
-      'tmpdir': os.tmpdir(),
-      'totalmem': os.totalmem(),
-      'uptime': os.uptime(),
-      'cpuUsage': os.loadavg()
-    }
-    this.res.send(infos)
+    var cpuStats = require('cpu-stats')
+
+    // the first argument is how long to sample for in ms.
+    // longer is more accurate but, you know, longer.
+    // if omitted, defaults to one second.
+    cpuStats(1000, (error, result) => {
+      console.error(error)
+      let infos = {
+        'architecture': os.arch(),
+        'cpus': os.cpus(),
+        'homedir': os.homedir(),
+        'hostname': os.hostname(),
+        'freemem': os.freemem(),
+        'networkInterfaces': os.networkInterfaces(),
+        'platform': os.platform(),
+        'release': os.release(),
+        'tmpdir': os.tmpdir(),
+        'totalmem': os.totalmem(),
+        'uptime': os.uptime(),
+        'cpuUsage': result
+      }
+      this.res.send(infos)
+    })
   }
 }
 
