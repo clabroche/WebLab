60d1f6b28191b9d29f66c6e6386e6ab9b2a735e7
xchopin <xchopin54@gmail.com>
2017-03-04 00:17:14 +0100

    Added child process (fork)

--- Slave/Controllers/AlgoController.js
+++ Slave/Controllers/AlgoController.js
@@ -7,11 +7,35 @@ let AlgoController = class AlgoController {
     this.res = res
     this.next = next
     this.controller = new Controller(req, res, next)
+    this.child = require('child_process').fork(`${__dirname}/AlgoChildProcess.js`)
   }
   launch () {
-    for (let i = 0; i < this.req.body.iteration; i++) {
-      eval(JSON.parse(this.req.body.algo))
-    }
+    this.child.send({ algorithm: JSON.parse(this.req.body.algo) })
+    this.child.on('message', (m) => {
+      console.log('Controller (parent) got :', m)
+    })
+
+    /**  let tmp = require('tmp')
+    let fs = require('fs')
+    let currentFile = require.main.filename.slice(0, -3)
+    let tmpFile = tmp.fileSync({ mode: '0644', prefix: 'algo-', postfix: '.js' })
+    fs.appendFile(tmpFile.name, new Buffer('let dependency = require("' + currentFile + '") \n' +
+      'const vm = dependency.nodeVM \n' +
+      'console.log(vm.run("return 150"'   + JSON.parse(this.req.body.algo)+  + '))'))
+
+   this.child.exec('node ' + tmpFile.name, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        return
+      }
+      console.log(stdout)
+    })
+
+   /** for (let i = 0; i < this.req.body.iteration; i++) {
+      console.log('Console of the algorithm : ' + )
+      let result = this.vm.run(JSON.parse(this.req.body.algo))
+      console.log(result)
+    } */
   }
 }
 
